ddef51bbabbc5cae336a016e8ea136817f5ad8ba
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-05-25 01:12:38 -0300

    feat: implement an interface to telnet service

---
+++ Infrastructure/Services/IoTDeviceConnectService.cs
@@ -0,0 +1,35 @@
+namespace Infrastructure.Service;
+public class IotDeviceConnectService
+{
+    private TelnetClient _telnetClient;
+
+    public void ConnectToDevice(string url)
+    {
+        _telnetClient = new TelnetClient();
+        _telnetClient.Connect(url, 23);
+    }
+
+    public void Login(string user, string password)
+    {
+        _telnetClient.SendCommand(user);
+        ReceiveData();
+        _telnetClient.SendCommand(password);
+        ReceiveData();
+
+    }
+
+    public void SendCommand(string command)
+    {
+        _telnetClient.SendCommand(command);
+    }
+
+    public string ReceiveData()
+    {
+        return _telnetClient.ReadData();
+    }
+
+    public void CloseConnection()
+    {
+        _telnetClient.Close();
+    }
+}
