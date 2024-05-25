8158812b04efb54883940d3ea08d72e8dedee678
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-05-25 01:14:11 -0300

    feat: create an CRUD device controller

---
+++ Web/Controllers/DeviceController.cs
@@ -0,0 +1,87 @@
+using Application;
+using Domain.Model;
+using Infrastructure.Service;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace IoTDeviceManagerService.Controllers;
+
+[ApiController]
+[Route("Devices")]
+public class DeviceController : ControllerBase
+{
+	private readonly DeviceService deviceService;
+
+	private readonly IotDeviceConnectService iotDeviceConnectService = new IotDeviceConnectService();
+
+	[ActivatorUtilitiesConstructor]
+	public DeviceController(
+		DeviceService service
+		)
+	{
+		deviceService = service;
+	}
+
+	[HttpGet]
+	[Route("")]
+	public async Task<IActionResult> GetDevices()
+	{
+		List<Device> devices = await deviceService.GetDevices();
+		return Ok(devices);
+	}
+
+	[HttpPost]
+	[Route("")]
+	public async Task<IActionResult> CreateDevice(Device device)
+	{
+		await deviceService.CreateDevice(device);
+		return CreatedAtAction(nameof(CreateDevice), device);
+
+	}
+
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetDevice(Guid id)
+	{
+		var device = await deviceService.GetDeviceById(id);
+		if (device == null)
+		{
+			return NotFound();
+		}
+		return Ok(device);
+	}
+
+	[HttpPut("{id}")]
+	public async Task<IActionResult> UpdateDevice(Guid id, Device device)
+	{
+		await deviceService.UpdateDevice(id, device);
+
+		return Ok(null);
+	}
+
+	[HttpDelete("{id}")]
+	public async Task<IActionResult> DeleteDevice(Guid id)
+	{
+		await deviceService.DeleteDevice(id);
+
+		return Ok(null);
+	}
+
+	[HttpPost("send-command")]
+	public async Task<IActionResult> SendDevice(Guid id, Command command)
+	{
+		var device = await deviceService.GetDeviceById(id);
+
+		if (device == null)
+		{
+			return NotFound();
+		}
+
+		iotDeviceConnectService.ConnectToDevice(device.Url);
+		iotDeviceConnectService.SendCommand("");
+		iotDeviceConnectService.SendCommand(command.command);
+		var dataReceived = iotDeviceConnectService.ReceiveData();
+		Console.WriteLine(dataReceived);
+
+		return Ok(dataReceived);
+	}
+}
