0cc5d933a663dcf9c75f45e462a2dcc0432faced
Vinicius Evangelista <vinicius.evangelista@cubos.academy>
2024-05-25 01:14:59 -0300

    feat: create an user create/authenticate controller

--- Web/Controllers/UserController.cs
+++ Web/Controllers/UserController.cs
@@ -1,4 +1,7 @@
+using System.Text;
 using Application;
+using Domain;
+using Domain.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IoTDeviceManagerService.Controllers;
@@ -8,17 +11,42 @@ namespace IoTDeviceManagerService.Controllers;
 public class UserController : ControllerBase
 {
     private readonly UserService service;
+    private readonly UserListProvider UserList;
 
-    public UserController(UserService userService)
+    public UserController(UserService userService, UserListProvider userList)
     {
         service = userService;
+        UserList = userList;
     }
 
-    [HttpPost]
+    [HttpPost("create")]
     public async Task<IActionResult> Create(string name, string email, string password)
     {
-        await service.Create(name, email,password);
+        if (UserList.Users.Find(user => user.Email == email) != null)
+        {
+            return BadRequest("Email already exist");
+        }
+
+        UserList.Users.Add(new User(name, email, password));
 
         return CreatedAtAction(nameof(Create), null);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(string email, string password)
+    {
+        var user = UserList.Users.Find(user => user.Email == email);
+
+        if (user == null)
+        {
+            return NotFound("Credentials are incorrect");
+        }
+
+        if (user.Password != password)
+        {
+            return BadRequest("Credentials are incorrect");
+        }
+
+        return Ok(Convert.ToBase64String(Encoding.ASCII.GetBytes(user.Id + ":" + user.Password)));
     }
 }
